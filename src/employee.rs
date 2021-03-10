4db4dd21bf515de754d19008ef8dadb9440ce0ff
tmsc4zht <79966690+tmsc4zht@users.noreply.github.com>
2021-03-10 21:06:59 +0900

    list_department実装

--- src/employee.rs
+++ src/employee.rs
@@ -21,6 +21,12 @@ impl Employee {
     self.data.get(department)
   }
 
+  fn list_department(&self) -> Vec<String> {
+    let result: Vec<&String> = self.data.keys().collect();
+    result.into_iter().map(|x| x.clone()).collect()
+  }
+}
+
 #[cfg(test)]
 mod test {
   use super::*;
@@ -50,5 +56,19 @@ mod test {
     let want = vec![String::from("Alice"), String::from("Bob")];
 
     assert_eq!(data.list_member("Engineering").unwrap(), &want);
+  }
+
+  #[test]
+  fn list_department_test() {
+    let mut data = Employee::new();
+
+    data.add("Sales", "Amir");
+    data.add("Engineering", "Bob");
+
+    let want = vec![String::from("Sales"), String::from("Engineering")];
+
+    data.list_department();
+
+    assert_eq!(data.list_department(), want);
   }
 }
